328a5849cf7f14c6bf80717b7ad04ade3f18889d
Hawleywoo <alhawley21@gmail.com>
2021-08-18 22:48:56 -0600

    Ts config notes

---
+++ ConfigProject/app/configTs.ts
@@ -0,0 +1,18 @@
+function startGame(){
+    var messagesElement = document.getElementById('messages');
+    messagesElement!.innerHTML = 'Welcome to the Math Game! Starting a new game...';
+}
+
+document.getElementById('startGame')!.addEventListener('click', startGame);
+
+// tsc CLI Options in ts docs has compiler options add to tsconfig.json
+// target is the which version of js to output
+// sourceMap used to debug ts ouput is running in browser
+// outDir is directory of output
+// strict true
+// watch leaves compiler running
+// files: [] is a Array of files to compile
+//  ! is a non null operator
+// in dev tools go to sources tab have all the files go into ts file add breakpoint
+
+// config inhertiance
